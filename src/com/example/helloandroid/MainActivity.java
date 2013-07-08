8e735fdcc5f32fa6c39291d464aaeba8b2339114
Daniel <danbadge@gmail.com>
2013-07-08 16:40:39 +0100

    add a few more albums and get left to right swipe working

--- src/com/example/helloandroid/MainActivity.java
+++ src/com/example/helloandroid/MainActivity.java
@@ -1,31 +1,63 @@
 package com.example.helloandroid;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+
 import com.loopj.android.image.SmartImageView;
 
-import android.os.Bundle;
-import android.app.Activity;
+public class MainActivity extends Activity implements OnClickListener {
+	private GestureDetector rightToLeftListener;
+	private SparseArray<String> classicAlbums = new SparseArray<String>();
+	private int counter = 1;
 
-public class MainActivity extends Activity {
+	public MainActivity() {
+		classicAlbums.put(1,"http://www.beatlesbible.com/wp/media/abbey_road.jpg");
+		classicAlbums.put(2,"http://www.alexandruvita.com/blog/wp-content/uploads/2010/05/Pink-Floyd-Dark-Side-of-t.jpg");
+		classicAlbums.put(3,"http://media-cache-ec3.pinimg.com/736x/8a/2a/fe/8a2afedf10058b46a20f36e947f0285a.jpg");
+		classicAlbums.put(4,"http://media-cache-ak0.pinimg.com/736x/20/30/6b/20306b23c66354a9bac17435e1bae419.jpg");
+		classicAlbums.put(5,"http://www.sydbarrettpinkfloyd.com/uploaded_images/wish-you-were-here-pink-floyd-737825.jpg");
+		classicAlbums.put(6,"http://media-cache-ec3.pinimg.com/736x/71/ba/99/71ba99d75b08229790ea373eca8e149e.jpg");
+		classicAlbums.put(7,"http://cdn.stereogum.com/files/2012/10/radiohead-the-bends.jpeg");
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-    	//requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
-    	super.onCreate(savedInstanceState);
-        
-        setContentView(R.layout.activity_main);
-       // getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
-       
-    	SmartImageView image = (SmartImageView)findViewById(R.id.beatles);
-    	String imageUrl = "http://www.beatlesbible.com/wp/media/abbey_road.jpg";
-    	image.setImageUrl(imageUrl);        	
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
+		final SmartImageView image = (SmartImageView) findViewById(R.id.beatles);
+		String imageUrl = classicAlbums.get(counter);
+		image.setImageUrl(imageUrl);
 
-    /*@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }*/
-    
+		rightToLeftListener = new GestureDetector(this, new RightToLeftSwipeListener());
+
+		image.setOnClickListener(MainActivity.this);
+		
+		image.setOnTouchListener(new OnTouchListener() {
+			public boolean onTouch(View v, MotionEvent event) {
+				if (rightToLeftListener.onTouchEvent(event)) {
+					if (counter == classicAlbums.size())
+						counter = 1;
+					else
+						counter++;
+					
+					String newImageUrl = classicAlbums.get(counter);
+					image.setImageUrl(newImageUrl);
+					return true;
+				}
+				return false;
+			}
+		});
+	}
+
+	@Override
+	public void onClick(View view) {
+		// do nothing
+	}
 }
---
+++ src/com/example/helloandroid/RightToLeftSwipeListener.java
@@ -0,0 +1,32 @@
+package com.example.helloandroid;
+
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+
+public class RightToLeftSwipeListener extends SimpleOnGestureListener {
+	private final int DEFAULT_SWIPE_MIN_DISTANCE = 120;
+    private final int DEFAULT_SWIPE_THRESHOLD_VELOCITY = 200;
+	
+    @Override
+    public boolean onFling(MotionEvent eventOne, MotionEvent eventTwo, 
+                                    float velocityX, float velocityY) {
+    	return isLeftToRightSwipe(eventOne, eventTwo, velocityX);
+    }
+    
+    private boolean isLeftToRightSwipe(MotionEvent eventOne, MotionEvent eventTwo, float velocityX) {
+    	return isSwipe(eventOne.getX(), eventTwo.getX(), velocityX);
+    }
+    
+    private boolean isSwipe(float coordinateA, float coordinateB, float velocity) {
+        return isSwipeDistance(coordinateA, coordinateB)
+                && isSwipeSpeed(velocity);
+    }
+    
+    private boolean isSwipeDistance(float coordinateA, float coordinateB) {
+        return (coordinateA - coordinateB) > this.DEFAULT_SWIPE_MIN_DISTANCE;
+    }
+
+    private boolean isSwipeSpeed(float velocity) {
+        return Math.abs(velocity) > this.DEFAULT_SWIPE_THRESHOLD_VELOCITY;
+    }
+}
